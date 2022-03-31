5110816e59c1c23e4cdeb9a6511631f5c828d147
unknown <danterkum16@gmail.com>
2022-03-31 23:59:07 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# list = ['a','b','c','d','e','f','g','h','i']
+#
+#
+# def printlist(list):
+#     new_list = []
+#     for i in range(len(list)):
+#         if list[i] in list[2::3]:
+#             new_list.append(list[i])
+#     return print(new_list)
+#
+#
+# printlist(list)
+
+
